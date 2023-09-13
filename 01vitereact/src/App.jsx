f8b7b099aa53906fe92667a1fecda11ede884e34
Muhammad Bilal <muhammadbilal254542@gmail.com>
2023-09-14 01:27:12 +0500

    create project with vite and react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1>Chai Aur React | Muhammad Bilal</h1>
+      <p>
+        In this First React Tuorial, I am Learning about how to create react app
+        with Vite also learn about the basic of file sturcture.
+      </p>
+    </>
+  );
+}
+
+export default App;
