908de157de1eec8b6f25ad70bcd3d2cd26359385
Muhammad Bilal <muhammadbilalmnaeem@gmail.com>
2023-09-14 01:30:00 +0500

    create project

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1>Chai Aur React | Muhammad Bilal</h1>
+      <p>
+        In this First React Tuorial, I am Learning about how to create react app
+        with create-react-app utility and also learn about the basic of file
+        sturcture.
+      </p>
+    </>
+  );
+}
+
+export default App;
